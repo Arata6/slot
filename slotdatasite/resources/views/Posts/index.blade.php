ac1ba95704b02b36f93d224e4e6b257dc6e91fca
Arata6 <24W604D@shinshu-u.ac.jp>
2024-07-26 23:27:02 +0900

    Procfile for Heroku

---
+++ slotdatasite/resources/views/Posts/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Slot data</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+       
+    </head>
+    <body class="antialiased">
+       <h1>Slot data</h1>
+       <div class='posts'>
+           <div class='post'>
+               <h2 class='title'>北信エリア</h2>
+               <p class='body'>北信エリアの情報はこちら</p>
+           </div>
+       </div>
+    </body>
+</html>
